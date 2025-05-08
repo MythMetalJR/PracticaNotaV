eff0c4e689d179742d6dff37d384a86318e43f38
Josue Rodriguez <wilsonrodri3000@gmail.com>
2025-05-08 17:14:22 -0500

    Add files via upload

---
+++ JosueAlejandroRodriguez2025.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.josuealejandrorodriguez2025;
+
+/**
+ *
+ * @author User
+ */
+public class JosueAlejandroRodriguez2025 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
